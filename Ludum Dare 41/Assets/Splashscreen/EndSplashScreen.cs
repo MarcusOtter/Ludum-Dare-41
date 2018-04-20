518626840adb44d0dad0529cf3c18a27d51d126b
Marcus Otterström <marcus.otterstrom@gmail.com>
2018-04-21 00:10:27 +0200

    Added Unity project
    Contains an intro screen & an empty placeholder scene for main menu.

---
+++ Ludum Dare 41/Assets/Splashscreen/EndSplashScreen.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndSplashScreen : MonoBehaviour
+{
+    private void ChangeScene()
+    {
+        // Changes to the next scene in the hierarchy
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
